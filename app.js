11c17e04e33d978e0096b584a80501aef1e60b76
Ethan Vargas <20080406@H2WHK12TQ6NW.tld>
2022-08-26 08:45:03 -0500

    New logic for section 3

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-function getDatat() {
-    var place = document.getElementById("place").nodeValue;
-    var person = document.getElementById("person").nodeValue;
-    var verb = document.getElementById("verb").nodeValue;
-    var adj = document.getElementById("adj").nodeValue;
-    var adverb = document.getElementById("adverb").nodeValue;
-    
-    console.log(place, person, verb, adj, adverb)
-    return [place, person, verb, adj, adverb]
+function getData() {
+    var place = document.getElementById("place").value;
+    var person = document.getElementById("person").value;
+    var verb = document.getElementById("verb").value;
+    var adj = document.getElementById("adj").value;
+    var adverb = document.getElementById("adverb").value;
+
+    document.getElementById("sentence").innerHTML = sentences(verb, adverb, person, place, adj);
 }
 
-function sentences() {
+//              0                       1                   2               3               4
+
+function sentences(verb, adverb, person, place, adj) {
     sentenceArray = [`Today, I had to ${verb} ${adverb}. But right in the mid]dle of it, ${person} came with this ${adj} book and wanted to go to ${place}`, `${person} was in ${place} trying to ${verb}. However, ${person} left a ${adj} shirt behind, while crying ${adverb}.`, `It's been a long time since ${person} decided to ${verb} in ${place}. What some people said was that ${person} screamed "This strawberry is ${adj}!! We thought ${person} went crazy.`]
+
+
+ranN = Math.floor(Math.random() * sentenceArray.length)
+// 0 to 2
+
+return sentenceArray[ranN]
 }
+
+console.log(sentences())
