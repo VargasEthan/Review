885768909b5e239f0cd4f29d7e94df173f807c14
Ethan Vargas <20080406@H2WHK12TQ6NW.tld>
2022-09-08 09:03:28 -0500

    New

--- app.js
+++ app.js
@@ -21,3 +21,69 @@ return sentenceArray[ranN]
 }
 
 console.log(sentences())
+
+
+var wordArr = [
+    {
+        word: "dog",
+        hint: "company you keep at home", 
+        
+    },
+    {
+        word: "place",
+        hint: "somewhere you are at any time", 
+    },
+    {
+        word: "tree",
+        hint: "it grows from the ground",
+    },
+    {
+        word: "computer",
+        hint: "you are using it right now",
+    }
+]
+
+var rN = Math.floor(Math.random() * wordArr.length);
+
+var word = wordArr[rN].word;
+var hint = wordArr[rN].hint;
+
+document.getElementById("word").innerHTML = word;
+document.getElementById("hint").innerHTML = hint;
+
+var w = document.getElementById("word");
+
+var splitWord = word.split("");
+
+var emptySplitWord = [];
+
+var correct = [];
+
+var counter = word.length + 2
+
+console.log(splitWord)
+
+for (i = 0; i < splitWord.length ; i++) {
+    emptySplitWord[i] = "x"
+    console.log(emptySplitWord);
+    document.getElementById("word").innerHTML = emptySplitWord.
+    join(" ")
+}
+
+function enterLetter() {
+    var letter = document.getElementById("letter").value
+   
+    for ( x = 0; x < splitWord.lenght; x++)
+    if (letter == splitWord[i]) {
+        emptySplitWord[x] = letter;
+        w.innerHTML = emptySplitWord.join(" ")
+        document.getElementById("letter").value = "";
+        correct.push(1);
+    } else {
+        document.getElementById("letter").value = "";
+    }
+}
+
+if (correct.length == emptySplitWord.length) {
+    alert("THE END")
+}
